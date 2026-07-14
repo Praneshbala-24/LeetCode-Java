204de918e31e96a7c08c476dfe88595577704672
Pranesh Bala <praneshbala24@gmail.com>
2026-07-14 08:18:19 +0530

    Time: 1 ms (99.91%) | Memory: 43.7 MB (38.28%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -2,13 +2,10 @@ class Solution {
     public String addBinary(String a, String b) {
         int lenA = a.length();
         int lenB = b.length();
-
         if(lenA < lenB) return addBinary(b , a);
-
         char[] result = new char[lenA + 1];
         int carry = 0;
         int k = lenA;
-
         for(int i = lenA - 1, j = lenB - 1; i >= 0; i--){
             carry += a.charAt(i) - '0';
             if(j >= 0){
@@ -17,16 +14,10 @@ class Solution {
             result[k--] = (char)('0' + (carry & 1));
             carry >>= 1;    
         }
-
         if(carry == 1){
             result[k] = '1';
             return new String(result);
         }
-
         return new String(result,1,lenA);
-
-
-
-
     }
 }
